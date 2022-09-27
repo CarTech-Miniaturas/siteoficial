b30964d6c941179d99fc9ad1ae9dd55b1e146eb5
Lucas Poeys Carpenter de Sant'Anna <92745389+Lucas-Poeys@users.noreply.github.com>
2022-09-27 08:12:50 -0300

    atualização
    validação do login

--- JS/script.js
+++ JS/script.js
@@ -47,3 +47,45 @@ function valemail(form, num) {
         }
     }
 }
+function mostrar() {
+    let senha = document.getElementById("senha-login");
+    let checkbox = document.getElementById("mostrar-senha");
+    if (checkbox.checked) {
+        senha.type = "text"
+    }
+    else {
+        senha.type = "password"
+    }
+}
+
+function logar() {
+    let email = document.getElementById("email-login").value;
+    let senha = document.getElementById("senha-login").value;
+    let vemail = document.getElementById("valilogin-email");
+    let vsenha = document.getElementById("valilogin-senha");
+    let vinval = document.getElementById("valilogin-inval");
+    if (email == "") {
+        vemail.style.display = "block";
+        vsenha.style.display = "none";
+        vinval.style.display = "none";
+        return false
+    }
+    else if (senha == "") {
+        vsenha.style.display = "block";
+        vemail.style.display = "none";
+        vinval.style.display = "none";
+        return false
+    }
+    else {
+        vemail.style.display = "none";
+        vsenha.style.display = "none";
+        
+        if (email == "usuario@email.com" && senha == "user") {
+            vinval.style.display = "none";
+        }
+        else {
+            vinval.style.display = "block";
+            return false
+        }
+    }
+}
